6aa117adba055fc898e73aa5c64a16d6994a81c1
ccuts996 <90644291+ccuts996@users.noreply.github.com>
2021-09-13 21:57:24 -0400

    Initial commit

---
+++ src/main/java/ex13.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ex13 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String principle;
+        System.out.print(" What is the principle? ");
+
+        principle = input.next();
+
+        Integer a = Integer.parseInt(principle);
+
+        String interestrate;
+        System.out.print(" What is the rate of interest? ");
+
+        interestrate = input.next();
+
+        Double b = Double.parseDouble(interestrate);
+
+
+        String years;
+        System.out.print(" How long is the interest accrued for? ");
+
+        years = input.next();
+
+        Integer c = Integer.parseInt(years);
+
+        String compound ;
+        System.out.print(" How many times compounded? ");
+
+        compound = input.next();
+
+        Integer d = Integer.parseInt(compound);
+
+
+
+        double interest = b / 100;
+        double y = a * (1 + b/d);
+        double x = (d * c);
+        double amount = Math.pow(y, x);
+
+        System.out.print("After " +c+ " years at " + interest + "% compounded " + d +" times, the investment will be worth $" + amount);
+    }
+}
